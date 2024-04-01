441aa42a5a7e7253de3c536dca3de03f05e35664
Bastheblackdachshund <144002097+Bastheblackdachshund@users.noreply.github.com>
2024-04-01 18:10:15 +0200

    Cameras
    heb veel cameras toegevoegd en een manier om tussen ze te switchen

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject FrontC;
+    public GameObject WheelC;
+    public GameObject TopC;
+    public GameObject MapC;
+    public GameObject MidRC;
+    public GameObject MidLC;
+    public GameObject BackC;
 
-    // Update is called once per frame
+    private int cam = 1;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown("up"))
+        {
+            cam++;
+        }
+        if (cam == 1)
+        {
+            MapC.SetActive(true);
+            BackC.SetActive(false);
+        }
+        if (cam == 2)
+        {
+            MapC.SetActive(false);
+            TopC.SetActive(true);
+        }
+        if (cam == 3)
+        {
+            TopC.SetActive(false);
+            WheelC.SetActive(true);
+        }
+        if (cam == 4)
+        {
+            WheelC.SetActive(false);
+            FrontC.SetActive(true);
+        }
+        if (cam == 5)
+        {
+            FrontC.SetActive(false);
+            MidRC.SetActive(true);
+        }
+        if (cam == 6)
+        {
+            MidRC.SetActive(false);
+            MidLC.SetActive(true);
+        }
+        if (cam == 7)
+        {
+            MidLC.SetActive(false);
+            BackC.SetActive(true);
+        }
+        if (cam >= 8) { cam = 1; }
     }
 }
